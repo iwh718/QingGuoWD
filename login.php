8ce0e9945e67d3efdbe03111f22f5395963d88e3
iwh718 <630020826@qq.com>
2019-03-20 22:19:03 +0800

    调整UI
    更简洁，便捷。

--- login.php
+++ login.php
@@ -1,163 +1,162 @@
-
 <?php
 
 /**
  * 青果教务系统登录
  * @author IWH 2868579699@qq.com
- *  
+ *
  **/
 
 error_reporting(0);
 session_start();
 require('./SHDM/simple_html_dom.php');
 header("Content-type:text/html;charset=utf-8");
- 
+
 
 /**
  * 登录函数
  * @param String $loginUrl 登录链接
- * @param String $__VIWESTATE  DOM状态值
-  */
- 
- function Login($loginUrl = 'http://218.22.58.76:2346/_data/login_new.aspx'){
-   
+ */
 
-$referer="http://218.22.58.76:2346/_data/login_new.aspx";
-$pcInfo="Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0Windows NT 10.0; WOW645.0 (Windows) SN:NULL";        
-$data = array (
-    'fgfggfdgtyuuyyuuckjg'=>$_POST['fgfggfdgtyuuyyuuckjg'],
-    'dsdsdsdsdxcxdfgfg'=> $_POST['dsdsdsdsdxcxdfgfg'],
-    'txt_asmcdefsddsd' => $_POST['txt_asmcdefsddsd'], 
-    'txt_pewerwedsdfsdff' => '',
-    'txt_sdertfgsadscxcadsads'=>'',
-    'typeName' => iconv( "UTF-8", "gb2312//IGNORE","学生"),
-    'sbtState'=>'',
-    'Sel_Type'=>'STU',
-    'pcInfo'=>$pcInfo,
-    '__VIEWSTATE'=>$_SESSION['__VIEWSTATE']
-); 
+function Login($loginUrl = 'http://218.22.58.76:2346/_data/login_new.aspx')
+{
 
-$data = http_build_query($data);
-$header=array();
-$header[]="User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0Windows NT 10.0; WOW645.0 (Windows) SN:NULL";
-$header[]="Host:218.22.58.76:2346";
-$header[] = "Content-Type: application/x-www-form-urlencoded";
-$header[] = "Referer: http://218.22.58.76:2346/_data/login_new.aspx";
-$header[] = "Origin: http://218.22.58.76:2346";
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, $loginUrl); 
-curl_setopt($ch,CURLOPT_HTTPHEADER,$header);//设置请求头，青果会通过请求头来验证请求是否为爬虫
-curl_setopt ($ch, CURLOPT_POST, 1 );//请求方式为post
-curl_setopt($ch, CURLOPT_HEADER, true); //返回结果带头信息
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_AUTOREFERER,true);
-curl_setopt($ch, CURLOPT_TIMEOUT,30); 
-curl_setopt($ch, CURLOPT_COOKIE, $_SESSION['cookie']); //带上sessionCookie值
-curl_setopt ($ch, CURLOPT_POSTFIELDS, $data );//写入post信息
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1); 
+
+    $pcInfo = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0Windows NT 10.0; WOW645.0 (Windows) SN:NULL";
+    $data = array(
+        'fgfggfdgtyuuyyuuckjg' => $_POST['fgfggfdgtyuuyyuuckjg'],
+        'dsdsdsdsdxcxdfgfg' => $_POST['dsdsdsdsdxcxdfgfg'],
+        'txt_asmcdefsddsd' => $_POST['txt_asmcdefsddsd'],
+        'txt_pewerwedsdfsdff' => '',
+        'txt_sdertfgsadscxcadsads' => '',
+        'typeName' => iconv("UTF-8", "gb2312//IGNORE", "学生"),
+        'sbtState' => '',
+        'Sel_Type' => 'STU',
+        'pcInfo' => $pcInfo,
+        '__VIEWSTATE' => $_SESSION['__VIEWSTATE']
+    );
+
+    $data = http_build_query($data);
+    $header = array();
+    $header[] = "User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0Windows NT 10.0; WOW645.0 (Windows) SN:NULL";
+    $header[] = "Host:218.22.58.76:2346";
+    $header[] = "Content-Type: application/x-www-form-urlencoded";
+    $header[] = "Referer: http://218.22.58.76:2346/_data/login_new.aspx";
+    $header[] = "Origin: http://218.22.58.76:2346";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $loginUrl);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);//设置请求头，青果会通过请求头来验证请求是否为爬虫
+    curl_setopt($ch, CURLOPT_POST, 1);//请求方式为post
+    curl_setopt($ch, CURLOPT_HEADER, true); //返回结果带头信息
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_COOKIE, $_SESSION['cookie']); //带上sessionCookie值
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);//写入post信息
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 //写入文件，检查是否登录成功
-$result=curl_exec($ch);
-$file = dirname(__FILE__)."/result.html";
-$fp = fopen($file,"w");
-fwrite($fp,$result);
-fwrite($fp, '结束');
-fclose($fp);
-$result = iconv('gbk', 'utf-8', $result);
-$nums=substr_count($result,"验证码错误");
-$nums2 = substr_count($result,"帐号已被锁定");
-$nums3 = substr_count($result,"帐号或密码不正确");
-if($nums >=1 ){
-   echo 
-   "<script>
+    $result = curl_exec($ch);
+    $file = dirname(__FILE__) . "/result.html";
+    $fp = fopen($file, "w");
+    fwrite($fp, $result);
+    fwrite($fp, '结束');
+    fclose($fp);
+    $result = iconv('gbk', 'utf-8', $result);
+    $nums = substr_count($result, "验证码错误");
+    $nums2 = substr_count($result, "帐号已被锁定");
+    $nums3 = substr_count($result, "帐号或密码不正确");
+    if ($nums >= 1) {
+        echo
+        "<script>
    alert('验证码错误！！');
    location.href = './WdClass.php';
    </script>";
-  die();
-}elseif($nums2 >=1){
-   echo 
-   "<script>
-   alert('iwh提示：由于你的账号或密码多次输入错误，已被锁定，去文鼎楼处理吧！');
+        die();
+    } elseif ($nums2 >= 1) {
+        echo
+        "<script>
+   alert('提示：由于你的账号或密码多次输入错误，已被锁定，去文鼎楼处理吧！');
     location.href = './WdClass.php';
    </script>";
-  die();
-}elseif($nums3 >=1){
-    echo 
-   "<script>
-   alert('iwh温馨提示：账号或者密码错误，请不要超过三次！');
+        die();
+    } elseif ($nums3 >= 1) {
+        echo
+        "<script>
+   alert('温馨提示：账号或者密码错误，请不要超过三次！');
     location.href = './WdClass.php';
    </script>";
-  die();
-}
+        die();
+    }
 
 
 }
 
 /**
  * 获取成绩
- * @param String $scoresUrl 查询分数url 
+ * @param String $scoresUrl 查询分数url
  * @return Array 返回分数数组
  */
-function getScores($scoresUrl = "http://218.22.58.76:2346/xscj/Stu_cjfb_rpt.aspx"){
-$dom = new simple_html_dom(); //new simple_html_dom对象
-$dataScore = array (
-    'SelXNXQ'=>'0',
-    'submit'=> iconv( "UTF-8", "gb2312//IGNORE","检索"),
-   
-); 
+function getScores($scoresUrl = "http://218.22.58.76:2346/xscj/Stu_cjfb_rpt.aspx")
+{
+    $dom = new simple_html_dom(); //new simple_html_dom对象
+    $dataScore = array(
+        'SelXNXQ' => '0',
+        'submit' => iconv("UTF-8", "gb2312//IGNORE", "检索"),
+
+    );
 
 
-$headerScore[]="User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0Windows NT 10.0; WOW645.0 (Windows) SN:NULL";
-$headerScore[]="Host:218.22.58.76:2346";
-$headerScore[] = "Content-Type: application/x-www-form-urlencoded";
-$headerScore[] = "Referer: http://218.22.58.76:2346/xscj/Stu_cjfb.aspx";
-$headerScore[] = "Origin: http://218.22.58.76:2346";
-$dataScore[] = http_build_query($dataScore);
-$chScore = curl_init(); 
-curl_setopt($chScore, CURLOPT_URL, $scoresUrl); 
-curl_setopt($chScore,CURLOPT_HTTPHEADER,$headerScore);//设置请求头，青果会通过请求头来验证请求是否为爬虫
-curl_setopt ($chScore, CURLOPT_POST, 1 );//请求方式为post
-curl_setopt($chScore, CURLOPT_HEADER, false); //返回结果带头信息
-curl_setopt($chScore, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($chScore, CURLOPT_TIMEOUT,30); 
-curl_setopt($chScore, CURLOPT_COOKIE, $_SESSION['cookie']); //带上session值
-curl_setopt ($chScore, CURLOPT_POSTFIELDS, $dataScore );//写入post信息
-curl_setopt($chScore, CURLOPT_FOLLOWLOCATION,1); 
+    $headerScore[] = "User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0Windows NT 10.0; WOW645.0 (Windows) SN:NULL";
+    $headerScore[] = "Host:218.22.58.76:2346";
+    $headerScore[] = "Content-Type: application/x-www-form-urlencoded";
+    $headerScore[] = "Referer: http://218.22.58.76:2346/xscj/Stu_cjfb.aspx";
+    $headerScore[] = "Origin: http://218.22.58.76:2346";
+    $dataScore[] = http_build_query($dataScore);
+    $chScore = curl_init();
+    curl_setopt($chScore, CURLOPT_URL, $scoresUrl);
+    curl_setopt($chScore, CURLOPT_HTTPHEADER, $headerScore);//设置请求头，青果会通过请求头来验证请求是否为爬虫
+    curl_setopt($chScore, CURLOPT_POST, 1);//请求方式为post
+    curl_setopt($chScore, CURLOPT_HEADER, false); //返回结果带头信息
+    curl_setopt($chScore, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($chScore, CURLOPT_TIMEOUT, 30);
+    curl_setopt($chScore, CURLOPT_COOKIE, $_SESSION['cookie']); //带上session值
+    curl_setopt($chScore, CURLOPT_POSTFIELDS, $dataScore);//写入post信息
+    curl_setopt($chScore, CURLOPT_FOLLOWLOCATION, 1);
 
-$resultScore=curl_exec($chScore);
-$resultScore = iconv('gbk', 'utf-8', $resultScore);
-$fileScore = dirname(__FILE__)."/resultScore.html";
-$fpScore = fopen($fileScore,"w");
-fwrite($fpScore,$resultScore);
-fwrite($fpScore, 'ScoresOver');
-fclose($fpScore);
+    $resultScore = curl_exec($chScore);
+    $resultScore = iconv('gbk', 'utf-8', $resultScore);
+    $fileScore = dirname(__FILE__) . "/resultScore.html";
+    $fpScore = fopen($fileScore, "w");
+    fwrite($fpScore, $resultScore);
+    fwrite($fpScore, 'ScoresOver');
+    fclose($fpScore);
 
-$dom->load($resultScore);
-$scoresList = $dom->find('#ID_Table',0);
-$scoresArray;
-$_SESSION['maxBad']  = 0;
-foreach ($scoresList->find('tr') as $keyOuter => $valueOuter) {
+    $dom->load($resultScore);
+    $scoresList = $dom->find('#ID_Table', 0);
+    $scoresArray;
+    $_SESSION['maxBad'] = 0;
+    foreach ($scoresList->find('tr') as $keyOuter => $valueOuter) {
         //获取历史成绩
         $scoresArray[] = array(
-          'semester'=>$valueOuter->find('td',0)->plaintext,
-          'course'=> preg_replace('/\[.*?\]/',"",$valueOuter->find('td',1)->plaintext),
-          'credits'=> $valueOuter->find('td',2)->plaintext,
-          'course_type'=> $valueOuter->find('td',3)->plaintext,
-          'exam_type'=> $valueOuter->find('td',4)->plaintext,
-          'scores'=> $valueOuter->find('td',11)->innertext
-          
+            'semester' => $valueOuter->find('td', 0)->plaintext,
+            'course' => preg_replace('/\[.*?\]/', "", $valueOuter->find('td', 1)->plaintext),
+            'credits' => $valueOuter->find('td', 2)->plaintext,
+            'course_type' => $valueOuter->find('td', 3)->plaintext,
+            'exam_type' => $valueOuter->find('td', 4)->plaintext,
+            'scores' => $valueOuter->find('td', 11)->innertext
+
         );
         //检测一共挂了多少科目
-        if((int)($valueOuter->find('td',11)->innertext) < 60 ){
-          ++$_SESSION['maxBad'];
+        if ((int)($valueOuter->find('td', 11)->innertext) < 60) {
+            ++$_SESSION['maxBad'];
         }
 
     }
-   //echo  $_SESSION['maxBad'];
+    //echo  $_SESSION['maxBad'];
     //die();
 
-array_splice($scoresArray, count($scoresArray)-2, 2);
-$_SESSION['maxBad'] -= 2;
-return $scoresArray;
+    array_splice($scoresArray, count($scoresArray) - 2, 2);
+    $_SESSION['maxBad'] -= 2;
+    return $scoresArray;
 
 }
 
@@ -165,18 +164,19 @@ return $scoresArray;
  * 输出成绩表单
  * @return [type] [description]
  */
-function echoScores($scoresArray){
-$note = "";
-if($_SESSION['maxBad'] == 0){
-  $note = "你很优秀啊，目前没有挂科哦！";
-}elseif($_SESSION['maxBad'] <=3){
-  $note  = "注意补考哦，再接再厉！";
-}elseif($_SESSION['maxBad'] >=4){
-  $note = "同学，该关注关注学业啦！";
-}
+function echoScores($scoresArray)
+{
+    $note = "";
+    if ($_SESSION['maxBad'] == 0) {
+        $note = "你很优秀啊，目前没有挂科哦！";
+    } elseif ($_SESSION['maxBad'] <= 3) {
+        $note = "注意补考哦，再接再厉！";
+    } elseif ($_SESSION['maxBad'] >= 4) {
+        $note = "同学，该关注关注学业啦！";
+    }
 
 
-  print <<< SCORE
+    print <<< SCORE
 <html><head>
     <title>一键查成绩</title>
       <meta charset="utf-8">
@@ -185,40 +185,72 @@ if($_SESSION['maxBad'] == 0){
        <script src="https://cdn.bootcss.com/bootstrap/3.3.2/js/bootstrap.js"></script>
      <link href="https://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.css" rel="stylesheet"> 
       <link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <style type="text/css">
+body{
+  background:#3CB371
+}
+.sc-box{
+  background:#fff;
+  padding: 10px;
+  border-bottom: 1px solid rgba(0,0,0,0.1);
+  
+  margin: 10px;
 
-  </head>
-  <body>
-  <div class="">
-<table class="table table-bordered table-condensed">
-  <caption class="text-center">入学以来成绩<br>你一共挂了 {$_SESSION['maxBad']} 个课程,{$note} &nbsp;&nbsp;<span class='fa fa-times-circle-o fa-2x' onclick="location.href = './WdClass.php';"></span></caption>
-  <thead>
-    <tr>
-      <th>学期</th>
-      <th>课程</th>
-      <th>学分</th>
-      <th>类别</th>
-      <th>方式</th>
-      <th>成绩</th>
-    </tr>
-  </thead>
-  <tbody>
-SCORE;
-   foreach ($scoresArray as $element=> $value) {
-print <<<TRS
- <tr>
-      <td style="max-width:100px;min-width:80px;word-break: keep-all;white-space:nowrap;text-overflow:ellipsis;overflow:hidden;">{$value['semester']}</td>
-      <td title="{$value['course']}" style="max-width:100px;min-width:90px;word-break: keep-all;white-space:nowrap;text-overflow:ellipsis;overflow:hidden;">{$value['course']}</td>
-      <td>{$value['credits']}</td>
-      <td style="max-width:50px;min-width:50px;word-break: keep-all;white-space:nowrap;text-overflow:ellipsis;overflow:hidden;">{$value['course_type']}</td>
-      <td>{$value['exam_type']}</td>
-      <td>{$value['scores']}</td>
-    </tr>
-TRS;
+}
+.sc-head{
+    font-weight: bolder;
+}
+.panel-heading{
+  background:     #3CB371;
+  color:#fff;
 }
 
-print <<<SCORE2
-  </tbody>
-</table>
+
+</style>
+<script type="text/javascript">
+
+$(function(){
+  $('.sc-scores').each(function(){
+    if(parseInt($(this).data('scores')) >= 60){
+        $(this).css({
+          color:"#5CACEE"
+        })
+    }else{
+      $(this).css({
+          color:"#FF4500"
+        })
+    }
+  })
+  
+})
+
+</script>
+  </head>
+
+  <body>
+
+<div class="">
+  <div  class="text-center panel-heading">同学，入学以来成绩<br>你一共挂了 {$_SESSION['maxBad']} 个课程,{$note} &nbsp;&nbsp;<span class='fa fa-times-circle-o fa-2x' onclick="location.href = './WdClass.php';"></span></div>
+ <div class="panel-body">
+
+SCORE;
+    foreach ($scoresArray as $element => $value) {
+        print <<<TRS
+<div class="row sc-box">
+          <div class="col-sm-12 text-center sc-head"><p>{$value['semester']}</p></div>
+          <div class="col-sm-12 sc-h"><p>{$value['course']}</p></div>
+          <div class="col-sm-6 col-md-6 com-xs-6"><p>学分：{$value['credits']}</p></div>
+          <div class="col-sm-6 col-md-6 col-xs-6"><p>课程类别：{$value['course_type']}</p></div>
+          <div class="col-sm-6 col-md-6 col-xs-6"><p>考试方式：{$value['exam_type']}</p></div>
+          <div class="col-sm-6 sc-scores col-md-6 col-xs-6" data-scores = '{$value['scores']}' ><p>分数：{$value['scores']}</p></div>
+</div>
+ 
+TRS;
+    }
+
+    print <<<SCORE2
+
+</div>
 </div>
 </body>
 </html>
@@ -229,18 +261,13 @@ SCORE2;
 
 
 //Cookie
-$cookie=$_SESSION['cookie']; 
+$cookie = $_SESSION['cookie'];
 //DOM状态值
-$__VIEWSTATE=$_SESSION['__VIEWSTATE'];
+$__VIEWSTATE = $_SESSION['__VIEWSTATE'];
 //调用登录
 Login();
 //输出成绩
 echoScores(getScores());
-
-
-
-
-
 
 
 ?>
--- wdClass.php
+++ wdClass.php
@@ -1,88 +1,160 @@
+<html>
+<head>
+    <title>文院一键查成绩</title>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.2/js/bootstrap.js"></script>
+    <script src="./md5.js"></script>
+    <script src="./check.js"></script>
+    <link href="https://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <style type="text/css">
+        .sign_box {
 
-<html><head>
-		<title>文院一键查成绩</title>
-			<meta charset="utf-8">
-		 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-	     <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
-	     <script src="https://cdn.bootcss.com/bootstrap/3.3.2/js/bootstrap.js"></script>
-	     <script src="./md5.js"></script>
-	     <script src="./check.js"></script>
-		 <link href="https://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.css" rel="stylesheet">	
-		 <link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-		 <style type="text/css">
-.sign_box{
-	box-shadow: 2px 2px 2px 2px rgba(0,0,0,0.4);
-	padding: 10px;
-	border-radius: 10px;
-	margin: 10px;
-	background:#fff;
-}
-body{
-	background:  #595959;
-	padding-top: 50px;
-}
-.loginButton{
-	border-radius: 50%;
-	height: 50px;
-	width: 50px;
-	box-shadow: 2px 2px 2px 2px rgba(0,0,0,0.2);
-}
-.form-group,.form-control,input{
-	border-radius: 0!important;
-	border-top: none;
-	border-left: none;
-	box-shadow: none;
-	border-right:none;
-}
-.form-control:focus {
-    border-color: #66afe9;
-    outline: 0;
-    -webkit-box-shadow: none;
-     box-shadow: none; 
-}
+            padding: 10px;
+            border-radius: 10px;
+            margin: 10px;
+            background: #fff;
+        }
+
+        body {
+            background: #fff;
+
+        }
+
+        .loginButton {
+            border-radius: 50%;
+            height: 50px;
+            width: 50px;
+            box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.1);
+        }
+
+        .form-group, .form-control, input {
+            border-radius: 0 !important;
+            border-top: none;
+            border-left: none;
+            box-shadow: none;
+            border-right: none;
+        }
+
+        .form-control:focus {
+            border-color: #66afe9;
+            outline: 0;
+            -webkit-box-shadow: none;
+            box-shadow: none;
+        }
+
+        .logo {
+            width: 20%;
+            margin-left: 40%;
+            margin-right: 40%;
+            border-radius: 50%;
+        }
+
+        .h4 {
+            width: 100%;
+            text-align: center;
+        }
+
+        .info {
+            display: none;
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.8);
+        }
+
+        .info-content {
+
+            position: fixed;
+            width: 80%;
+            left: 10%;
+            right: 25%;
+            top: 20%;
+            border-radius: 10px;
+            padding: 5px;
+            background: #ffffff;
+        }
+
+        #info-close {
+            text-align: center;
+            display: block;
+            width: 100%;
+        }
 
 
+    </style>
+    <script type="text/javascript">
 
-		</style>
 
-	</head>
-	<body >
-		<div class="container">
-			<div class="sign_box ">
-				<div class="h4 text-center">文达学院一键查成绩 <small>版本：v1.0 | By IWH</small></div>
-			<form name="Logon" method="post" action="./login.php" id="Logon"  autocomplete="off" >
-				<div class="form-group">
-    				<label for="name">学号</label>
-    					<input name="txt_asmcdefsddsd" id="txt_asmcdefsddsd" class="form-control"  type="text">
-  				</div>
-  				<div class="form-group">
-    				<label for="name">密码</label>
-    					<input class="form-control" id="txt_pewerwedsdfsdff"  name="txt_pewerwedsdfsdff"  onblur="chkpwd(this)" onkeyup="chkpwd(this)" type="password">	
-  				</div>
-			
-			<input id="dsdsdsdsdxcxdfgfg" name="dsdsdsdsdxcxdfgfg" type="hidden">
-			<input id="fgfggfdgtyuuyyuuckjg" name="fgfggfdgtyuuyyuuckjg" type="hidden">			
-			<div class="form-group">
-    				<label for="name">输入验证码</label>
-    					<input class="form-control" id="txt_sdertfgsadscxcadsads" name="txt_sdertfgsadscxcadsads"  onblur="chkyzm(this)" onkeyup="chkyzm(this)">
-  				</div>
-  				<div class="form-group">
-    					<img id="imgCode" src="./getValidate.php" onclick="changeValidateCode(this)" alt="单击可更换图片！" title="点击更换">		
-  				</div>
-			
-			<div class="form-group">
-    			<button class="btn btn-primary center-block loginButton" type="submit"><span class="fa fa-chevron-right"></span></button>	
-  			</div>
-							
-			
-		</form>		
-	</div>
-			
-		</div>
-		<footer>
-			<div class="text-center text-dark">版本：v1.0 | By IWH</div>
-		</footer>			
-	</body>
-	 <script src="./check.js"></script>
+        $(function () {
+            $('#info').click(function () {
+                $('.info').show(300);
+            });
+            $('#info-close').click(function () {
+                $('.info').hide(300);
+            });
+
+        })
+    </script>
+</head>
+<body>
+<div class="info">
+    <div class="info-content">
+        <p>1.该应用不保留您的账户信息。</p>
+        <p>2.应用数据来自文达教务系统。</p>
+        <p>3.如果查询失败，可能接口失效了，或者网络阻塞。</p>
+        <p>4.有建议反馈QQ群：959281938</p>
+        <p>5.目前喜鹊儿软件已经支持文达学院，如需更好的体验，请自行下载。</p>
+        <span class="fa fa-close  fa-2x" id="info-close"></span>
+    </div>
+
+</div>
+<div class="container">
+    <div class="sign_box ">
+        <img class="logo" src="./logo.jpg"/>
+
+        <div class="h4 text-enter">文达学院一键查成绩 <span class="fa fa-question-circle-o" id="info"></span></div>
+        <!---->
+        <form name="Logon" method="post" id="Logon" action="./login.php" autocomplete="off">
+            <div class="form-group">
+                <label for="name">学号</label>
+                <input name="txt_asmcdefsddsd" id="txt_asmcdefsddsd" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label for="name">密码</label>
+                <input class="form-control" id="txt_pewerwedsdfsdff" name="txt_pewerwedsdfsdff" onblur="chkpwd(this)"
+                       onkeyup="chkpwd(this)" type="password">
+            </div>
+
+            <input id="dsdsdsdsdxcxdfgfg" name="dsdsdsdsdxcxdfgfg" type="hidden">
+            <input id="fgfggfdgtyuuyyuuckjg" name="fgfggfdgtyuuyyuuckjg" type="hidden">
+            <div class="form-group">
+                <label for="name">输入验证码</label>
+                <input class="form-control" id="txt_sdertfgsadscxcadsads" name="txt_sdertfgsadscxcadsads"
+                       onblur="chkyzm(this)" onkeyup="chkyzm(this)">
+            </div>
+            <div class="form-group">
+                <img id="imgCode" src="./getValidate.php" onclick="changeValidateCode(this)" alt="单击可更换图片！"
+                     title="点击更换">
+            </div>
+
+            <div class="form-group">
+                <button class="btn btn-primary center-block loginButton" type="submit"><span
+                            class="fa fa-chevron-right"></span></button>
+            </div>
+
+
+        </form>
+    </div>
+
+</div>
+<footer>
+    <div class="text-center text-dark">版本：v1.0 | By IWH</div>
+</footer>
+</body>
+<script src="./check.js"></script>
 
 </html>
